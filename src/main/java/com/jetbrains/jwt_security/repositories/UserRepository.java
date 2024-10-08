487ff8fdd5b3b23dbd0f950cc9a13062ce98dbfa
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-10-08 15:39:58 +0200

    User Repository

---
+++ src/main/java/com/jetbrains/jwt_security/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.jetbrains.jwt_security.repositories;
+
+import com.jetbrains.jwt_security.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmailOrUsername(String email, String username);
+}
