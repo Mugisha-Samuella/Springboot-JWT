492db40a0f150189ab06dd8d1c5ef9ab0973de37
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-10-09 11:09:44 +0200

    JWT token, service & dependencies

---
+++ src/main/java/com/jetbrains/jwt_security/config/JwtService.java
@@ -0,0 +1,11 @@
+package com.jetbrains.jwt_security.config;
+
+import org.springframework.stereotype.Service;
+
+public class JwtService {
+
+    @Service
+    public String extractUsername(String token) {
+        return null;
+    }
+}
