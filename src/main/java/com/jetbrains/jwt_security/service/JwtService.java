5bc296e23603b8b31ee0d8bc127a6091f066338c
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-10-14 22:27:03 +0200

    Extracting Claims, getSignInKey

--- src/main/java/com/jetbrains/jwt_security/service/JwtService.java
+++ src/main/java/com/jetbrains/jwt_security/service/JwtService.java
@@ -1,9 +1,42 @@
 package com.jetbrains.jwt_security.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.function.Function;
+
+import static javax.crypto.Cipher.SECRET_KEY;
+
 public class JwtService {
+
+    private static final String SECRET_KEY = "825D53842226EC3F2C2763ECA55C2";
 
 
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(KeyBytes);
+
     }
 }
